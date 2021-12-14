2e075ec28309b6ba501a7d99a59b62a6aeffd558
ShamerD <shomax31@gmail.com>
2021-12-14 22:22:49 +0300

    remove unnecessary data handling

--- src/utils/data/__init__.py
+++ src/utils/data/__init__.py
@@ -1,17 +1,15 @@
-from .collator import Batch, LJSpeechCollator, TextCollator
-from .datasets import LJSpeechDataset, TextDataset
+from .collator import Batch, LJSpeechCollator
+from .datasets import LJSpeechDataset
 from .featurizer import MelSpectrogramConfig, MelSpectrogram
 from .lj_trainval_split import LJ_DATA_DIR
 from .loader import get_dataloaders
 
 __all__ = [
     "LJSpeechDataset",
-    "TextDataset",
     "MelSpectrogramConfig",
     "MelSpectrogram",
     "Batch",
     "LJSpeechCollator",
-    "TextCollator",
     "get_dataloaders",
     "LJ_DATA_DIR"
 ]
--- src/utils/data/loader.py
+++ src/utils/data/loader.py
@@ -4,7 +4,7 @@ import numpy as np
 from numpy.random import shuffle
 from torch.utils.data import DataLoader, Subset
 
-from src.utils.data import LJSpeechDataset, LJSpeechCollator, TextDataset, TextCollator
+from src.utils.data import LJSpeechDataset, LJSpeechCollator
 
 
 def get_dataloaders(data_config: Dict):
@@ -44,11 +44,5 @@ def get_dataloaders(data_config: Dict):
                                         collate_fn=LJSpeechCollator(),
                                         shuffle=False,
                                         num_workers=num_workers)
-
-    inference_dataset = TextDataset()
-    dataloaders['inference'] = DataLoader(inference_dataset,
-                                          batch_size=1,
-                                          collate_fn=TextCollator(),
-                                          shuffle=False)
 
     return dataloaders
