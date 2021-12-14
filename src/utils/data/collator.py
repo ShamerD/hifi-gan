714709cb304707f56ed1ac1a9cf2a2e49d7cfcae
ShamerD <shomax31@gmail.com>
2021-12-14 23:38:30 +0300

    collator now stores everything needed

--- src/utils/data/collator.py
+++ src/utils/data/collator.py
@@ -13,11 +13,36 @@ class Batch:
     spec: Optional[torch.Tensor] = None         # [B, n_mels, T]
     spec_gen: Optional[torch.Tensor] = None     # [B, n_mels, T]
 
+    # these contain outputs of every discriminator sub-block
+    disc_pred: Optional[List[torch.Tensor]] = None
+    disc_pred_gen: Optional[List[torch.Tensor]] = None
+
+    # these contain outputs of every discriminator layer
+    disc_features: Optional[List[torch.Tensor]] = None
+    disc_features_gen: Optional[List[torch.Tensor]] = None
+
+    # we put losses here to simplify its calculation and logging
+    # generator losses
+    generator_loss: Optional[torch.Tensor] = None
+    spec_loss: Optional[torch.Tensor] = None
+    adv_gen_loss: Optional[torch.Tensor] = None
+    feature_loss: Optional[torch.Tensor] = None
+
+    # discriminator losses
+    discriminator_loss: Optional[torch.Tensor] = None
+
     def to(self, device: torch.device) -> 'Batch':
         for field in fields(self):
+            if field.name.endswith('loss'):
+                continue
             attr = getattr(self, field.name)
-            if attr is not None:
-                setattr(self, field.name, attr.to(device))
+            if attr is None:
+                continue
+            if field.name.startswith('disc'):
+                attr = [t.to(device) for t in attr]
+            else:
+                attr = attr.to(device)
+            setattr(self, field.name, attr)
 
         return self
 
