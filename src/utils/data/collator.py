bab50a9dab7d0961abc4a96c11e78f46c29faf21
ShamerD <shomax31@gmail.com>
2021-12-14 22:20:57 +0300

    simplify data handling

--- src/utils/data/collator.py
+++ src/utils/data/collator.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, fields
-from typing import Tuple, Optional, List
+from typing import Optional, List
 
 import torch
 from torch.nn.utils.rnn import pad_sequence
@@ -7,32 +7,14 @@ from torch.nn.utils.rnn import pad_sequence
 
 @dataclass
 class Batch:
-    waveform: Optional[torch.Tensor]                    # [B, L]
-    waveform_length: Optional[torch.Tensor]             # [B]
+    wav: Optional[torch.Tensor] = None          # [B, L]
+    wav_gen: Optional[torch.Tensor] = None      # [B, L']
 
-    transcript: List[str]
-    tokens: torch.Tensor                                # [B, N]
-    token_lengths: torch.Tensor                         # [B]
-
-    durations: Optional[torch.Tensor] = None            # [B, N]
-    durations_pred: Optional[torch.Tensor] = None       # [B, N]
-
-    mels: Optional[torch.Tensor] = None                 # [B, n_mels, T]
-    mels_pred: Optional[torch.Tensor] = None            # [B, n_mels, T']
-    mels_length: Optional[torch.Tensor] = None          # [B]
-    mels_pred_length: Optional[torch.Tensor] = None     # [B]
-
-    mel_loss: Optional[torch.Tensor] = None
-    dur_loss: Optional[torch.Tensor] = None
-    loss: Optional[torch.Tensor] = None
+    spec: Optional[torch.Tensor] = None         # [B, n_mels, T]
+    spec_gen: Optional[torch.Tensor] = None     # [B, n_mels, T]
 
     def to(self, device: torch.device) -> 'Batch':
         for field in fields(self):
-            if field.name in ["mel_loss", "dur_loss", "loss", "transcript"]:
-                continue
-            if field.name in ["tokens", "token_lengths"]:
-                setattr(self, field.name, getattr(self, field.name).to(device))
-                continue
             attr = getattr(self, field.name)
             if attr is not None:
                 setattr(self, field.name, attr.to(device))
@@ -41,34 +23,11 @@ class Batch:
 
 
 class LJSpeechCollator:
-
-    def __call__(self, instances: List[Tuple]) -> Batch:
-        waveform, waveform_length, transcript, tokens, token_lengths = list(
-            zip(*instances)
-        )
+    def __call__(self, instances: List) -> Batch:
+        waveform = instances
 
         waveform = pad_sequence([
             waveform_[0] for waveform_ in waveform
         ]).transpose(0, 1)
-        waveform_length = torch.cat(waveform_length)
 
-        tokens = pad_sequence([
-            tokens_[0] for tokens_ in tokens
-        ]).transpose(0, 1)
-        token_lengths = torch.cat(token_lengths)
-
-        return Batch(waveform, waveform_length, transcript, tokens, token_lengths)
-
-
-class TextCollator:
-    def __call__(self, instances: List[Tuple]) -> Batch:
-        transcript, tokens, token_lengths = list(
-            zip(*instances)
-        )
-
-        tokens = pad_sequence([
-            tokens_[0] for tokens_ in tokens
-        ]).transpose(0, 1)
-        token_lengths = torch.cat(token_lengths)
-
-        return Batch(None, None, transcript, tokens, token_lengths)
+        return Batch(waveform)
--- src/utils/data/datasets.py
+++ src/utils/data/datasets.py
@@ -1,86 +1,17 @@
-import unicodedata
-from typing import List, Optional
-
-import torch
 import torchaudio
-from torch.utils.data import Dataset
 
 from src.utils import DATA_DIR
-
-
-def remove_accents(text: str):
-    """
-    :param text: single text possibly containing non-ascii symbols (eg. Müller)
-    :return: normalized text with accents removed
-
-    LJSpeech dataset contains 19 entries with accented text.
-    They can cause trouble, because 2 tokenizers (dataset's and aligner's) handle accented texts differently:
-    dataset's tokenizer simply ignores them, aligner's treat them as <unk>.
-    This leads to wrong alignment if not runtime error (if entry is batch's longest text)
-    """
-    return "".join([c for c in unicodedata.normalize("NFD", text) if not unicodedata.combining(c)])
 
 
 class LJSpeechDataset(torchaudio.datasets.LJSPEECH):
     def __init__(self):
         DATA_DIR.mkdir(parents=True, exist_ok=True)
         super().__init__(root=DATA_DIR, download=True)
-        self._tokenizer = torchaudio.pipelines.TACOTRON2_GRIFFINLIM_CHAR_LJSPEECH.get_text_processor()
 
     def __getitem__(self, index: int):
-        waveform, _, _, transcript = super().__getitem__(index)
-        waveform_length = torch.tensor([waveform.shape[-1]]).int()
+        waveform, _, _, _ = super().__getitem__(index)
 
-        transcript = remove_accents(transcript)
-        tokens, token_lengths = self._tokenizer(transcript)
-        transcript = self.decode(tokens, token_lengths)[0]
-
-        return waveform, waveform_length, transcript, tokens, token_lengths
-
-    def decode(self, tokens, lengths):
-        result = []
-        for tokens_, length in zip(tokens, lengths):
-            text = "".join([
-                self._tokenizer.tokens[token]
-                for token in tokens_[:length]
-            ])
-            result.append(text)
-        return result
-
-
-TEXT_EXAMPLES = [
-    "A defibrillator is a device that gives a high energy electric shock to the heart of someone who is in cardiac arrest",
-    "Massachusetts Institute of Technology may be best known for its math, science and engineering education",
-    "Wasserstein distance or Kantorovich Rubinstein metric is a distance function defined between probability distributions on a given metric space"
-]
-
-
-class TextDataset(Dataset):
-    def __init__(self, texts: Optional[List[str]] = None):
-        super().__init__()
-        self.texts = texts if texts is not None else TEXT_EXAMPLES
-        self._tokenizer = torchaudio.pipelines.TACOTRON2_GRIFFINLIM_CHAR_LJSPEECH.get_text_processor()
-
-    def __getitem__(self, index):
-        transcript = self.texts[index]
-        transcript = remove_accents(transcript)
-        tokens, token_lengths = self._tokenizer(transcript)
-        transcript = self.decode(tokens, token_lengths)[0]
-
-        return transcript, tokens, token_lengths
-
-    def __len__(self):
-        return len(self.texts)
-
-    def decode(self, tokens, lengths):
-        result = []
-        for tokens_, length in zip(tokens, lengths):
-            text = "".join([
-                self._tokenizer.tokens[token]
-                for token in tokens_[:length]
-            ])
-            result.append(text)
-        return result
+        return waveform
 
 
 if __name__ == "__main__":
