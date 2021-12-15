34e6d557340422203cc93ac4bc1dd882b2934345
ShamerD <shomax31@gmail.com>
2021-12-15 21:52:27 +0300

    added models to init

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,5 +1,9 @@
 from .default_config import ModelConfig
+from .generator import HiFiGenerator
+from .discriminator import HiFiDiscriminator
 
 __all__ = [
-    "ModelConfig"
+    "ModelConfig",
+    "HiFiGenerator",
+    "HiFiDiscriminator"
 ]
