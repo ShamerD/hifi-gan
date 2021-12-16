645e0b74a48e965d5641699fa711b96cbcb06ef0
ShamerD <shomax31@gmail.com>
2021-12-16 16:47:32 +0300

    rounding wavs to avoid mishaping in loss

--- src/utils/data/datasets.py
+++ src/utils/data/datasets.py
@@ -4,12 +4,15 @@ from src.utils import DATA_DIR
 
 
 class LJSpeechDataset(torchaudio.datasets.LJSPEECH):
-    def __init__(self):
+    def __init__(self, wav_round: int = 256):
         DATA_DIR.mkdir(parents=True, exist_ok=True)
         super().__init__(root=DATA_DIR, download=True)
+        self.wav_round = wav_round
 
     def __getitem__(self, index: int):
         waveform, _, _, _ = super().__getitem__(index)
+        if waveform.size(2) % self.wav_round != 0:
+            waveform = waveform[:, :, :(waveform.size(2) - self.wav_round)]
 
         return waveform
 
